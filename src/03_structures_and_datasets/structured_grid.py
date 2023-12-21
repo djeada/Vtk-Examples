0d6393040dffd8e80a7b9b33b8b9762d5c5c3442
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-21 19:50:40 +0100

    Update structured_grid.py

--- src/03_structures_and_datasets/structured_grid.py
+++ src/03_structures_and_datasets/structured_grid.py
@@ -7,26 +7,49 @@ import numpy as np
 
 from src.simple_pipeline import VisualisationPipeline
 
-# Define dimensions
-nx, ny, nz = 6, 6, 2
+def create_structured_grid(nx, ny, nz):
+    """
+    Create a structured grid with specified dimensions.
 
-# Create a structured grid
-structuredGrid = vtk.vtkStructuredGrid()
-structuredGrid.SetDimensions(nx, ny, nz)
+    Args:
+    nx, ny, nz (int): Dimensions of the grid in the x, y, and z directions.
 
-# Create points
-points = vtk.vtkPoints()
-for k in range(nz):
-    for j in range(ny):
-        for i in range(nx):
-            points.InsertNextPoint(i, j, k)
+    Returns:
+    vtkStructuredGrid: A structured grid with the specified dimensions.
+    """
+    structuredGrid = vtk.vtkStructuredGrid()
+    structuredGrid.SetDimensions(nx, ny, nz)
 
-# Set the points to the grid
-structuredGrid.SetPoints(points)
+    points = vtk.vtkPoints()
+    for k in range(nz):
+        for j in range(ny):
+            for i in range(nx):
+                points.InsertNextPoint(i, j, k)
+    
+    structuredGrid.SetPoints(points)
+    return structuredGrid
 
-# Create a mapper and actor for visualization
-mapper = vtk.vtkDataSetMapper()
-mapper.SetInputData(structuredGrid)
+def visualize_structured_grid(structuredGrid):
+    """
+    Visualize the given structured grid using a predefined visualization pipeline.
 
-pipeline = VisualisationPipeline(mappers=[mapper], edges_visible=True)
-pipeline.run()
+    Args:
+    structuredGrid (vtkStructuredGrid): The structured grid to be visualized.
+    """
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(structuredGrid)
+
+    # Configure and run the visualization pipeline
+    pipeline = VisualisationPipeline(mappers=[mapper], edges_visible=True)
+    pipeline.run()
+
+def main():
+    # Define dimensions for the structured grid
+    nx, ny, nz = 6, 6, 2
+
+    # Create and visualize the structured grid
+    structuredGrid = create_structured_grid(nx, ny, nz)
+    visualize_structured_grid(structuredGrid)
+
+if __name__ == "__main__":
+    main()
