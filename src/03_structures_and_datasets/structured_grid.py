d2d220eed31849e9b435bceacee425114f6f7f7c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-21 20:00:28 +0100

    Update structured_grid.py

--- src/03_structures_and_datasets/structured_grid.py
+++ src/03_structures_and_datasets/structured_grid.py
@@ -1,5 +1,20 @@
 """
 Structured grids in VTK represent a mesh in which points are arranged in a regular lattice in 3D space. It is best suited for problems where the domain can be discretized as a regular grid. For example, in computational fluid dynamics, it is common to use a structured grid to represent the fluid domain.
+
+Workflow Overview:
+
+1. Structured Grid Creation (create_structured_grid):
+   - Initializes a vtkStructuredGrid with specified dimensions (nx, ny, nz), representing the grid's resolution in each direction.
+   - Populates the grid with points in a regular, lattice-like arrangement using vtkPoints, illustrating how to define spatial coordinates in a structured grid.
+   - Returns a fully defined vtkStructuredGrid, ready for visualization.
+
+2. Visualization (visualize_structured_grid):
+   - Takes the created structured grid as input and sets up a vtkDataSetMapper for it. The mapper translates the grid data into a format suitable for rendering.
+   - Utilizes a predefined 'VisualisationPipeline' to handle the rendering process. This pipeline is configured to make the edges of the grid visible, enhancing the perception of the grid structure in 3D space.
+
+3. Main Function (main):
+   - Defines the dimensions for the structured grid.
+   - Calls the grid creation and visualization functions, orchestrating the process from grid setup to rendering.
 """
 
 import vtk
