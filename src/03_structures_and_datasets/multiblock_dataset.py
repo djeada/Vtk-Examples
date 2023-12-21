8fb145aa3b83c65d461ec56cb7f1b33931b82499
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-21 19:45:26 +0100

    Update multiblock_dataset.py

--- src/03_structures_and_datasets/multiblock_dataset.py
+++ src/03_structures_and_datasets/multiblock_dataset.py
@@ -6,28 +6,73 @@ import vtk
 
 from src.simple_pipeline import VisualisationPipeline
 
-# Create a sphere source
-sphereSource = vtk.vtkSphereSource()
-sphereSource.Update()
 
-# Create a cylinder source
-cylinderSource = vtk.vtkCylinderSource()
-cylinderSource.Update()
+def create_geometric_source(source_type, **params):
+    """
+    Create and return a geometric source based on the specified type and parameters.
+    Supported types: 'Sphere', 'Cylinder', 'Cone', 'Cube'.
+    """
+    if source_type == "Sphere":
+        source = vtk.vtkSphereSource()
+        source.SetRadius(params.get("radius", 1.0))
+        source.SetCenter(params.get("center", [0.0, 0.0, 0.0]))
+    elif source_type == "Cylinder":
+        source = vtk.vtkCylinderSource()
+        source.SetHeight(params.get("height", 2.0))
+        source.SetRadius(params.get("radius", 0.5))
+    elif source_type == "Cone":
+        source = vtk.vtkConeSource()
+        source.SetHeight(params.get("height", 2.0))
+        source.SetRadius(params.get("radius", 0.5))
+    elif source_type == "Cube":
+        source = vtk.vtkCubeSource()
+        source.SetXLength(params.get("length", 1.0))
+        source.SetYLength(params.get("width", 1.0))
+        source.SetZLength(params.get("height", 1.0))
+    else:
+        raise ValueError("Unsupported source type")
 
-# Create a multiblock dataset and add the sphere and the cylinder
-multiBlock = vtk.vtkMultiBlockDataSet()
-multiBlock.SetNumberOfBlocks(2)
-multiBlock.SetBlock(0, sphereSource.GetOutput())
-multiBlock.SetBlock(1, cylinderSource.GetOutput())
+    source.Update()
+    return source.GetOutput()
 
-# Use vtkCompositeDataGeometryFilter to convert the multiblock dataset to polydata
-geometryFilter = vtk.vtkCompositeDataGeometryFilter()
-geometryFilter.SetInputDataObject(multiBlock)
-geometryFilter.Update()
+def create_multiblock_dataset(sources):
+    """
+    Create a multiblock dataset from a list of VTK data sources.
+    """
+    multiBlock = vtk.vtkMultiBlockDataSet()
+    multiBlock.SetNumberOfBlocks(len(sources))
+    for i, source in enumerate(sources):
+        multiBlock.SetBlock(i, source)
+    return multiBlock
 
-# Create a mapper and actor for the polydata
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(geometryFilter.GetOutputPort())
+def convert_to_polydata(multiBlock):
+    """
+    Convert the multiblock dataset to polydata.
+    """
+    geometryFilter = vtk.vtkCompositeDataGeometryFilter()
+    geometryFilter.SetInputDataObject(multiBlock)
+    geometryFilter.Update()
+    return geometryFilter.GetOutput()
 
-pipeline = VisualisationPipeline(mappers=[mapper], point_size=30)
-pipeline.run()
+def main():
+    # Create various geometric sources
+    sphere = create_geometric_source("Sphere", radius=1.0)
+    cylinder = create_geometric_source("Cylinder", height=2.0, radius=0.5)
+    cone = create_geometric_source("Cone", height=2.0, radius=0.5)
+    cube = create_geometric_source("Cube", length=1.0, width=1.0, height=1.0)
+
+    # Create a multiblock dataset containing all sources
+    sources = [sphere, cylinder, cone, cube]
+    multiBlock = create_multiblock_dataset(sources)
+    polydata = convert_to_polydata(multiBlock)
+
+    # Create a mapper
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(polydata.GetOutputPort())
+
+    # Run the visualization pipeline
+    pipeline = VisualisationPipeline(mappers=[mapper], point_size=30)
+    pipeline.run()
+
+if __name__ == "__main__":
+    main()
