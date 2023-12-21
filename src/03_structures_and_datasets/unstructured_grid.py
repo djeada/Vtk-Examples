16a9c5a2f6626a51cf09a53f0468eccd161ffbdd
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-21 19:52:25 +0100

    Update unstructured_grid.py

--- src/03_structures_and_datasets/unstructured_grid.py
+++ src/03_structures_and_datasets/unstructured_grid.py
@@ -1,69 +1,115 @@
+"""
+This module demonstrates the creation, manipulation, and visualization of an unstructured grid using the Visualization Toolkit (VTK). An unstructured grid is a versatile data structure in VTK that allows for representing complex geometries composed of various types of cells. In this example, a hexahedron and a tetrahedron are created, along with their associated points, and then combined into a single unstructured grid. Additionally, scalar and vector fields are attached to this grid to illustrate the concept of data association with grid points and cells.
+
+Key Components and Workflow:
+
+1. Hexahedron and Tetrahedron Creation:
+   - Hexahedron (vtkHexahedron) and tetrahedron (vtkTetra) cells are created to represent two different 3D geometric shapes.
+   - The point ids for each cell are set to define the geometry of these cells.
+
+2. Points Creation (vtkPoints):
+   - A vtkPoints object is created to store the coordinates of the vertices for both the hexahedron and tetrahedron.
+
+3. Unstructured Grid (vtkUnstructuredGrid):
+   - An unstructured grid is defined to hold the created cells.
+   - This grid type is chosen for its ability to handle diverse cell types within a single data structure.
+
+4. Scalar and Vector Fields:
+   - Scalar values (random) are attached to the points of the grid, demonstrating how to associate data with grid points.
+   - Vector values (random) are attached to the cells of the grid, showing data association with grid cells.
+
+5. Visualization:
+   - A vtkDataSetMapper is used to map the unstructured grid data to graphical primitives.
+   - The visualization pipeline, encapsulated in the 'VisualisationPipeline' class, is then utilized to render the grid with the scalar and vector fields. The pipeline is configured to highlight the edges of the grid and enhance the visual understanding of its structure.
+
+"""
+
 import vtk
 import numpy as np
-
 from src.simple_pipeline import VisualisationPipeline
 
-# Create a hexahedron
-hexahedron = vtk.vtkHexahedron()
-hexahedron.GetPointIds().SetId(0, 0)
-hexahedron.GetPointIds().SetId(1, 1)
-hexahedron.GetPointIds().SetId(2, 2)
-hexahedron.GetPointIds().SetId(3, 3)
-hexahedron.GetPointIds().SetId(4, 4)
-hexahedron.GetPointIds().SetId(5, 5)
-hexahedron.GetPointIds().SetId(6, 6)
-hexahedron.GetPointIds().SetId(7, 7)
+def create_hexahedron():
+    """
+    Create and return a hexahedron cell.
+    """
+    hexahedron = vtk.vtkHexahedron()
+    for i in range(8):
+        hexahedron.GetPointIds().SetId(i, i)
+    return hexahedron
 
-# Create a tetrahedron
-tetrahedron = vtk.vtkTetra()
-tetrahedron.GetPointIds().SetId(0, 8)
-tetrahedron.GetPointIds().SetId(1, 9)
-tetrahedron.GetPointIds().SetId(2, 10)
-tetrahedron.GetPointIds().SetId(3, 11)
+def create_tetrahedron():
+    """
+    Create and return a tetrahedron cell.
+    """
+    tetrahedron = vtk.vtkTetra()
+    for i in range(4):
+        tetrahedron.GetPointIds().SetId(i, i + 8)
+    return tetrahedron
 
-# Create the points for the two cells
-points = vtk.vtkPoints()
-points.InsertNextPoint(0, 0, 0)
-points.InsertNextPoint(1, 0, 0)
-points.InsertNextPoint(1, 1, 0)
-points.InsertNextPoint(0, 1, 0)
-points.InsertNextPoint(0, 0, 1)
-points.InsertNextPoint(1, 0, 1)
-points.InsertNextPoint(1, 1, 1)
-points.InsertNextPoint(0, 1, 1)
-points.InsertNextPoint(2, 2, 2)
-points.InsertNextPoint(2, 3, 2)
-points.InsertNextPoint(3, 2, 2)
-points.InsertNextPoint(2, 2, 3)
+def create_points():
+    """
+    Create and return vtkPoints for the hexahedron and tetrahedron.
+    """
+    points = vtk.vtkPoints()
+    # Hexahedron points
+    for i in range(8):
+        points.InsertNextPoint(i % 2, (i // 2) % 2, i // 4)
+    # Tetrahedron points
+    points.InsertNextPoint(2, 2, 2)
+    points.InsertNextPoint(2, 3, 2)
+    points.InsertNextPoint(3, 2, 2)
+    points.InsertNextPoint(2, 2, 3)
+    return points
 
-# Create an unstructured grid
-ugrid = vtk.vtkUnstructuredGrid()
-ugrid.SetPoints(points)
-ugrid.InsertNextCell(hexahedron.GetCellType(), hexahedron.GetPointIds())
-ugrid.InsertNextCell(tetrahedron.GetCellType(), tetrahedron.GetPointIds())
+def create_unstructured_grid(points, hexahedron, tetrahedron):
+    """
+    Create and return an unstructured grid containing the hexahedron and tetrahedron.
+    """
+    ugrid = vtk.vtkUnstructuredGrid()
+    ugrid.SetPoints(points)
+    ugrid.InsertNextCell(hexahedron.GetCellType(), hexahedron.GetPointIds())
+    ugrid.InsertNextCell(tetrahedron.GetCellType(), tetrahedron.GetPointIds())
+    return ugrid
 
-# Attach a scalar field to the points
-scalars = vtk.vtkFloatArray()
-scalars.SetName("Scalars")
-for i in range(points.GetNumberOfPoints()):
-    scalars.InsertNextValue(np.random.rand())
-ugrid.GetPointData().SetScalars(scalars)
+def attach_fields_to_grid(ugrid, points):
+    """
+    Attach scalar and vector fields to the unstructured grid.
+    """
+    # Attach a scalar field to the points
+    scalars = vtk.vtkFloatArray()
+    scalars.SetName("Scalars")
+    for i in range(points.GetNumberOfPoints()):
+        scalars.InsertNextValue(np.random.rand())
+    ugrid.GetPointData().SetScalars(scalars)
 
-# Attach a vector field to the cells
-vectors = vtk.vtkFloatArray()
-vectors.SetNumberOfComponents(3)
-vectors.SetName("Vectors")
-for i in range(ugrid.GetNumberOfCells()):
-    vectors.InsertNextTuple3(np.random.rand(), np.random.rand(), np.random.rand())
-ugrid.GetCellData().SetVectors(vectors)
+    # Attach a vector field to the cells
+    vectors = vtk.vtkFloatArray()
+    vectors.SetNumberOfComponents(3)
+    vectors.SetName("Vectors")
+    for i in range(ugrid.GetNumberOfCells()):
+        vectors.InsertNextTuple3(np.random.rand(), np.random.rand(), np.random.rand())
+    ugrid.GetCellData().SetVectors(vectors)
 
-# Visualizing the fields
-mapper = vtk.vtkDataSetMapper()
-mapper.SetInputData(ugrid)
-mapper.SetScalarModeToUsePointData()
-mapper.SetColorModeToMapScalars()
-mapper.SelectColorArray("Scalars")
+def visualize_unstructured_grid(ugrid):
+    """
+    Visualize the unstructured grid with scalar and vector fields.
+    """
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(ugrid)
+    mapper.SetScalarModeToUsePointData()
+    mapper.SetColorModeToMapScalars()
+    mapper.SelectColorArray("Scalars")
 
+    pipeline = VisualisationPipeline(mappers=[mapper], point_size=30)
+    pipeline.run()
 
-pipeline = VisualisationPipeline(mappers=[mapper], point_size=30)
-pipeline.run()
+def main():
+    hexahedron = create_hexahedron()
+    tetrahedron = create_tetrahedron()
+    points = create_points()
+    ugrid = create_unstructured_grid(points, hexahedron, tetrahedron)
+    attach_fields_to_grid(ugrid, points)
+    visualize_unstructured_grid(ugrid)
+
+if __name__ == "__main__":
+    main()
