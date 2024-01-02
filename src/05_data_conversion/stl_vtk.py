79b920600028cd626c44d8d4ffa0635a1b3f6b95
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-01-03 00:31:10 +0100

    Update stl_vtk.py

--- src/05_data_conversion/stl_vtk.py
+++ src/05_data_conversion/stl_vtk.py
@@ -1,14 +1,29 @@
 import vtk
+import logging
 from converter_interface import Converter
 
 
-class VTKtoSTLConverter(Converter):
+class VtkToStlConverter(Converter):
     def convert(self, input_filename: str, output_filename: str):
-        reader = vtk.vtkGenericDataObjectReader()
-        reader.SetFileName(input_filename)
-        reader.Update()
+        if not input_filename or not output_filename:
+            raise ValueError("Input and output filenames must be provided.")
 
-        writer = vtk.vtkSTLWriter()
-        writer.SetFileName(output_filename)
-        writer.SetInputConnection(reader.GetOutputPort())
-        writer.Write()
+        if not (input_filename.lower().endswith('.vtk') and output_filename.lower().endswith('.stl')):
+            raise ValueError("Invalid file extensions. Expected '.vtk' and '.stl'.")
+
+        try:
+            # Read VTK File
+            reader = vtk.vtkGenericDataObjectReader()
+            reader.SetFileName(input_filename)
+            reader.Update()
+
+            # Write STL File
+            writer = vtk.vtkSTLWriter()
+            writer.SetFileName(output_filename)
+            writer.SetInputConnection(reader.GetOutputPort())
+            writer.Write()
+            logging.info(f"Conversion successful: {input_filename} to {output_filename}")
+        except Exception as e:
+            logging.error(f"Error during conversion: {e}")
+            raise
+
