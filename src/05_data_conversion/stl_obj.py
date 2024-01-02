3ad2fd2e17a28e9da6365232da776cd0c6db1f8d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-01-03 00:29:46 +0100

    Update stl_obj.py

--- src/05_data_conversion/stl_obj.py
+++ src/05_data_conversion/stl_obj.py
@@ -2,13 +2,26 @@ import vtk
 from converter_interface import Converter
 
 
-class STLtoOBJConverter(Converter):
+class StlToObjConverter:
     def convert(self, input_filename: str, output_filename: str):
-        reader = vtk.vtkSTLReader()
-        reader.SetFileName(input_filename)
-        reader.Update()
+        if not input_filename or not output_filename:
+            raise ValueError("Input and output filenames must be provided.")
 
-        writer = vtk.vtkOBJWriter()
-        writer.SetFileName(output_filename)
-        writer.SetInputConnection(reader.GetOutputPort())
-        writer.Write()
+        if not (input_filename.lower().endswith('.stl') and output_filename.lower().endswith('.obj')):
+            raise ValueError("Invalid file extensions. Expected '.stl' and '.obj'.")
+
+        try:
+            # Read STL File
+            reader = vtk.vtkSTLReader()
+            reader.SetFileName(input_filename)
+            reader.Update()
+
+            # Write OBJ File
+            writer = vtk.vtkOBJWriter()
+            writer.SetFileName(output_filename)
+            writer.SetInputConnection(reader.GetOutputPort())
+            writer.Write()
+            logging.info(f"Conversion successful: {input_filename} to {output_filename}")
+        except Exception as e:
+            logging.error(f"Error during conversion: {e}")
+            raise
