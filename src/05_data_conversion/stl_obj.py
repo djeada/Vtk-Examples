5d7f415d861c536a75f3d6e60b439e2f3cef60c2
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-01-03 00:33:38 +0100

    Update stl_obj.py

--- src/05_data_conversion/stl_obj.py
+++ src/05_data_conversion/stl_obj.py
@@ -26,3 +26,28 @@ class StlToObjConverter(Converter):
         except Exception as e:
             logging.error(f"Error during conversion: {e}")
             raise
+
+class ObjToStlConverter(Converter):
+    def convert(self, input_filename: str, output_filename: str):
+        if not input_filename or not output_filename:
+            raise ValueError("Input and output filenames must be provided.")
+
+        if not (input_filename.lower().endswith('.obj') and output_filename.lower().endswith('.stl')):
+            raise ValueError("Invalid file extensions. Expected '.obj' and '.stl'.")
+
+        try:
+            # Read OBJ File
+            reader = vtk.vtkOBJReader()
+            reader.SetFileName(input_filename)
+            reader.Update()
+
+            # Write STL File
+            writer = vtk.vtkSTLWriter()
+            writer.SetFileName(output_filename)
+            writer.SetInputConnection(reader.GetOutputPort())
+            writer.Write()
+            logging.info(f"Conversion successful: {input_filename} to {output_filename}")
+        except Exception as e:
+            logging.error(f"Error during conversion: {e}")
+            raise
+
